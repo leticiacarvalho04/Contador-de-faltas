2e6a1e4c80b2f3648276e14f30922a8090ae1922
leticiacarvalho04 <110743347+leticiacarvalho04@users.noreply.github.com>
2023-07-12 21:45:12 -0300

    inserindo banco de dados

--- contador.py
+++ contador.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import mysql.connector
 
 materias = ['Banco de Dados Relacional',
             'Estrutura de Dados',
@@ -10,11 +11,65 @@ materias = ['Banco de Dados Relacional',
 
 faltas_por_materia = {materia: 0 for materia in materias}
 
+# Função para salvar as faltas no banco de dados MySQL
+def salvar_faltas(materia, faltas):
+    # Estabelecer conexão com o banco de dados MySQL
+    conexao = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="fatec23",
+        database="contador_faltas"
+    )
+
+    # Criar cursor para executar as consultas
+    cursor = conexao.cursor()
+
+    # Inserir ou atualizar as faltas no banco de dados
+    consulta = "INSERT INTO materia_falt (idcurso, nomecursos, faltas) VALUES (%s, %s, %s) ON DUPLICATE KEY UPDATE faltas = %s"
+    valores = (materias.index(materia) + 1, materia, faltas, faltas)
+    cursor.execute(consulta, valores)
+
+    # Commit para salvar as alterações no banco de dados
+    conexao.commit()
+
+    # Fechar cursor e conexão com o banco de dados
+    cursor.close()
+    conexao.close()
+
+# Função para carregar as faltas do banco de dados MySQL
+def carregar_faltas():
+    # Estabelecer conexão com o banco de dados MySQL
+    conexao = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="fatec23",
+        database="contador_faltas"
+    )
+
+    # Criar cursor para executar as consultas
+    cursor = conexao.cursor()
+
+    # Consultar as faltas no banco de dados
+    consulta = "SELECT nomecursos, faltas FROM materia_falt"
+    cursor.execute(consulta)
+
+    # Recuperar os dados e atualizar o dicionário de faltas
+    for materia, quantidade in cursor:
+        faltas_por_materia[materia] = quantidade
+
+    # Fechar cursor e conexão com o banco de dados
+    cursor.close()
+    conexao.close()
+
+# Função para adicionar faltas
 def adicionar_faltas(materia):
     falta = int(faltas_entry.get())
     faltas_por_materia[materia] += falta
+
+    salvar_faltas(materia, faltas_por_materia[materia])  # Salvar as faltas no banco de dados
+
     faltas_atual_label.config(text=f'Número de faltas atual: {faltas_por_materia[materia]}')
-    
+
     aviso = verificar_aviso(materia)
     aviso_label.config(text=aviso)
     faltas_entry.delete(0, tk.END)
@@ -22,7 +77,7 @@ def adicionar_faltas(materia):
 def verificar_aviso(materia):
     limite_faltas = 20
     total_faltas = faltas_por_materia[materia]
-    
+
     if total_faltas >= limite_faltas:
         return f'AVISO: Você já tem {total_faltas} faltas em {materia}. Considere seu limite de faltas!'
     else:
@@ -33,19 +88,16 @@ janela = tk.Tk()
 janela.title("Contador de Faltas")
 janela.geometry("1000x500")  # Definindo a geometria da janela
 
+# Carregar as faltas do banco de dados ao iniciar o programa
+carregar_faltas()
+
 # Criar frame principal
 frame_principal = tk.Frame(janela)
 frame_principal.pack(expand=True)
 
 # Criar componentes da interface
-boas_vindas = tk.Label(frame_principal, text='Bem vindo(a) ao meu', font=('Arial',10))
-nome_materia_label = tk.Label(frame_principal, text="Contador de faltas", font=('Arial',20))
-nome_materia_label.pack(pady=10)
-
-
 materia_label = tk.Label(frame_principal, text="Escolha a matéria:")
 materia_label.pack(pady=10)
-
 
 materia_var = tk.StringVar()
 materia_combobox = ttk.Combobox(frame_principal, textvariable=materia_var, state="readonly", width=30)
