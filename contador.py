5a6f2596254e1327ec2fe63992b9c278f8c5bbb0
leticiacarvalho04 <110743347+leticiacarvalho04@users.noreply.github.com>
2023-07-27 12:09:19 -0300

    adicionando modo escuro e arrumando o código todo

--- contador.py
+++ contador.py
@@ -1,126 +1,197 @@
+import mysql.connector
 import tkinter as tk
 from tkinter import ttk
-import mysql.connector
 
-materias = ['Banco de Dados Relacional',
-            'Estrutura de Dados',
-            'Engenharia de Dados',
-            'Desenvolvimento Web II',
-            'Técnicas de Programação',
-            'Matemática para programação']
+current_mode = 'dark'
 
-faltas_por_materia = {materia: 0 for materia in materias}
+def bg():
+    light = tk.PhotoImage(file='brightness.png')
+    dark = tk.PhotoImage(file='dark.png')
 
-# Função para salvar as faltas no banco de dados MySQL
-def salvar_faltas(materia, faltas):
-    # Estabelecer conexão com o banco de dados MySQL
-    conexao = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="fatec23",
-        database="contador_faltas"
-    )
+    dark_mode_button = tk.Button(root, command=toggle_dark_mode)
+    dark_mode_button.config(image=dark, bd=0, bg='#1d1d1d')
+    dark_mode_button.pack(pady=10)
 
-    # Criar cursor para executar as consultas
-    cursor = conexao.cursor()
+    tela = tk.Canvas(root, width=600, height=20, bg='#1d1d1d', bd=0, highlightthickness=0,
+                     relief='ridge')
+    tela.pack()
 
-    # Inserir ou atualizar as faltas no banco de dados
-    consulta = "INSERT INTO materia_falt (idcurso, nomecursos, faltas) VALUES (%s, %s, %s) ON DUPLICATE KEY UPDATE faltas = %s"
-    valores = (materias.index(materia) + 1, materia, faltas, faltas)
-    cursor.execute(consulta, valores)
+def toggle_dark_mode():
+    global current_mode
 
-    # Commit para salvar as alterações no banco de dados
-    conexao.commit()
-
-    # Fechar cursor e conexão com o banco de dados
-    cursor.close()
-    conexao.close()
-
-# Função para carregar as faltas do banco de dados MySQL
-def carregar_faltas():
-    # Estabelecer conexão com o banco de dados MySQL
-    conexao = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="fatec23",
-        database="contador_faltas"
-    )
-
-    # Criar cursor para executar as consultas
-    cursor = conexao.cursor()
-
-    # Consultar as faltas no banco de dados
-    consulta = "SELECT nomecursos, faltas FROM materia_falt"
-    cursor.execute(consulta)
-
-    # Recuperar os dados e atualizar o dicionário de faltas
-    for materia, quantidade in cursor:
-        faltas_por_materia[materia] = quantidade
-
-    # Fechar cursor e conexão com o banco de dados
-    cursor.close()
-    conexao.close()
-
-# Função para adicionar faltas
-def adicionar_faltas(materia):
-    falta = int(faltas_entry.get())
-    faltas_por_materia[materia] += falta
-
-    salvar_faltas(materia, faltas_por_materia[materia])  # Salvar as faltas no banco de dados
-
-    faltas_atual_label.config(text=f'Número de faltas atual: {faltas_por_materia[materia]}')
-
-    aviso = verificar_aviso(materia)
-    aviso_label.config(text=aviso)
-    faltas_entry.delete(0, tk.END)
-
-def verificar_aviso(materia):
-    limite_faltas = 20
-    total_faltas = faltas_por_materia[materia]
-
-    if total_faltas >= limite_faltas:
-        return f'AVISO: Você já tem {total_faltas} faltas em {materia}. Considere seu limite de faltas!'
+    if current_mode == 'dark':
+        root['bg'] = 'white'
+        style.configure("TLabel", background="lightgray", foreground="black", font=("Arial", 12))
+        style.configure("TButton", background="blue", foreground="white", font=("Arial", 10))
+        option_menu_1.configure(background="white", foreground="black", highlightbackground="white")
+        entry_faltas.configure(background="white", foreground="black", highlightbackground="white")
+        filter_button.configure(background="white", foreground="black", highlightbackground="white")
+        clear_button.configure(background="white", foreground="black", highlightbackground="white")
+        tree.configure(style="Light.Treeview")
+        dark_mode_button.config(image=light, bg='white')
+        current_mode = 'light'
     else:
-        return ""
+        root['bg'] = '#1d1d1d'
+        style.configure("TLabel", background="#1d1d1d", foreground="white", font=("Arial", 12))
+        style.configure("TButton", background="blue", foreground="white", font=("Arial", 10))
+        option_menu_1.configure(background="#1d1d1d", foreground="white", font=("Arial", 10))
+        entry_faltas.configure(background="#1d1d1d", foreground="white", highlightbackground="#1d1d1d")
+        filter_button.configure(background="#1d1d1d", foreground="white", highlightbackground="white")
+        clear_button.configure(background="#1d1d1d", foreground="white", highlightbackground="white")
+        tree.configure(style="Dark.Treeview")
+        dark_mode_button.config(image=dark, bg='#1d1d1d')
+        current_mode = 'dark'
 
-# Criar janela principal
-janela = tk.Tk()
-janela.title("Contador de Faltas")
-janela.geometry("1000x500")  # Definindo a geometria da janela
 
-# Carregar as faltas do banco de dados ao iniciar o programa
-carregar_faltas()
+def apply_light_mode():
+    style.configure("TLabel", background="lightgray", foreground="black", font=("Arial", 12))
+    style.configure("TButton", background="blue", foreground="white", font=("Arial", 12))
+    option_menu_1.configure(background="white", foreground="black", highlightbackground="white")
+    entry_faltas.configure(background="white", foreground="black", highlightbackground="white")
+    filter_button.configure(background='white', foreground='black', font=("Arial", 12))
+    clear_button.configure(background='white', foreground='black', font=("Arial", 12))
 
-# Criar frame principal
-frame_principal = tk.Frame(janela)
-frame_principal.pack(expand=True)
+def create_database_table():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="fatec23",
+    )
+    cur = connection.cursor()
+    
+    cur.execute("CREATE DATABASE IF NOT EXISTS contador")
+    cur.execute("USE contador")
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS materia_falta (
+            codigo INT AUTO_INCREMENT PRIMARY KEY,
+            materia VARCHAR(60),
+            falta INT NOT NULL
+        )
+    ''')
+    
+    connection.commit()
+    cur.close()
+    connection.close()
 
-# Criar componentes da interface
-materia_label = tk.Label(frame_principal, text="Escolha a matéria:")
-materia_label.pack(pady=10)
+def materias():
+    materia = combo_box_1.get()
+    faltas = int(entry_faltas.get())  # Convert the value to an integer
 
-materia_var = tk.StringVar()
-materia_combobox = ttk.Combobox(frame_principal, textvariable=materia_var, state="readonly", width=30)
-materia_combobox['values'] = tuple(materias)
-materia_combobox.pack(pady=5)
+    connection = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="fatec23",
+                database="contador"
+            )
+    cur = connection.cursor()
+    
+    # Verificar se a matéria já existe no banco de dados
+    query_select = "SELECT falta FROM materia_falta WHERE materia = %s"
+    cur.execute(query_select, (materia,))
+    result = cur.fetchone()
 
-faltas_label = tk.Label(frame_principal, text="Adicione o número de faltas:")
-faltas_label.pack()
+    if result:  # Se a matéria já existe, atualize o número de faltas
+        faltas += int(result[0])  # Converta o valor recuperado em um número inteiro e adicione
+        query_update = "UPDATE materia_falta SET falta = %s WHERE materia = %s"
+        cur.execute(query_update, (faltas, materia))
+    else:  # Se a matéria não existe, insira uma nova entrada
+        query_insert = "INSERT INTO materia_falta (materia, falta) VALUES (%s, %s)"
+        cur.execute(query_insert, (materia, faltas))
 
-faltas_entry = tk.Entry(frame_principal, width=30)
-faltas_entry.pack(pady=5)
+    connection.commit()
 
-adicionar_faltas_button = tk.Button(frame_principal, text="Adicionar Faltas", command=lambda: adicionar_faltas(materia_combobox.get()), width=30)
-adicionar_faltas_button.pack(pady=10)
+    cur.close()
+    connection.close()
 
-faltas_atual_label = tk.Label(frame_principal, text="Número de faltas atual:", width=30)
-faltas_atual_label.pack()
+    # Após inserir os dados, atualize a tabela
+    update_table()
 
-aviso_label = tk.Label(frame_principal, text="", width=30)
-aviso_label.pack(pady=10)
+def clear_materia():
+    materia = combo_box_1.get()
 
-# Centralizar os componentes verticalmente
-frame_principal.pack(expand=True, pady=100)
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="fatec23",
+        database="contador"
+    )
+    cur = connection.cursor()
 
-# Iniciar loop da interface gráfica
-janela.mainloop()
+    query = "DELETE FROM materia_falta WHERE materia = %s"
+    cur.execute(query, (materia,))
+
+    connection.commit()
+
+    cur.close()
+    connection.close()
+
+    update_table()
+
+
+def update_table():
+    connection = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="fatec23",
+                database="contador"
+            )
+    cur = connection.cursor()
+
+    cur.execute("SELECT materia, falta FROM materia_falta")
+    data = cur.fetchall()
+
+    # Limpar a tabela antes de atualizá-la
+    for row in tree.get_children():
+        tree.delete(row)
+
+    # Atualizar a tabela com os dados obtidos do banco de dados
+    for item in data:
+        tree.insert("", "end", values=item)
+
+    cur.close()
+    connection.close()
+
+root = tk.Tk()
+root.title("Contador de Faltas")
+root.geometry("600x600")
+
+# Estilos
+style = ttk.Style()
+style.configure("TLabel", background="lightgray", font=("Arial", 12))
+style.configure("TButton", background="blue", foreground="white", font=("Arial", 12))
+
+# Criar o botão de alternância (toggle) do modo escuro
+light = tk.PhotoImage(file='brightness.png')
+dark = tk.PhotoImage(file='dark.png')
+dark_mode_button = tk.Button(root, command=toggle_dark_mode, bd=0, bg='#1d1d1d')
+dark_mode_button.config(image=dark)
+dark_mode_button.pack(pady=10)
+
+#Seleção das matérias
+combo_box_1 = tk.StringVar(root)
+combo_box_1.set("Matérias")
+options_1 = ["Banco de Dados – Relacional", "Engenharia de Software II", "Estrutura de Dados", 'Desenvolvimento Web II','Técnicas de Programação',
+             'Matemática para Computação']
+option_menu_1 = tk.OptionMenu(root, combo_box_1, *options_1)
+option_menu_1.pack()
+
+entry_faltas = tk.Entry(root)
+entry_faltas.pack(padx=10)
+
+filter_button = tk.Button(root, text="Registrar Faltas", command=materias)
+filter_button.pack()
+
+# Criar a tabela
+tree = tk.ttk.Treeview(root, columns=("Matéria", "Faltas"), show="headings")
+tree.heading("Matéria", text="Matéria")
+tree.heading("Faltas", text="Faltas")
+tree.pack()
+
+# Atualizar a tabela inicialmente
+create_database_table()
+update_table()
+
+clear_button = tk.Button(root,text='Limpar a quantia de faltas',command=clear_materia)
+clear_button.pack(pady=10)
+
+root.mainloop()
